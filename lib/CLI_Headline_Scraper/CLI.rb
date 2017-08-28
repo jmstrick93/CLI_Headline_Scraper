a4a73b0a5f41320e098564d3b1eef40dad74e79d
Jim Stricker <jmstricker93@gmail.com>
2017-08-28 19:44:12 -0400

    got CLIHeadlineScraper::CLI instance method #valid_selection? working

--- lib/CLI_Headline_Scraper/CLI.rb
+++ lib/CLI_Headline_Scraper/CLI.rb
@@ -77,19 +77,48 @@ class CLIHeadlineScraper::CLI
 
   end
 
-  def select_item #returns an array where arr[0] is the network name and arr[1] is the article number/
-    puts "To go to a story, type the network name and then the article number (e.g., BBC 2)"
+  def valid_selection?(selection)
 
-    selection = gets.strip.split(" ")
-    if selection.length == 2
-      selection[1] = selection[1].to_i
+    if selection == nil
+      false
+    elsif selection.length == 0
+      false
+    elsif selection.length == 2
+      if selection[1].to_i == 0
+        false
+      elsif selection[1].to_i > 3
+        false
+      else
+        true
+      end
     elsif selection.length > 2
-      puts "invalid entry"
-      select_item
+      false
+    else
+      true
     end
 
-    selection
+  end
 
+
+  def select_item #returns an array where arr[0] is the network name and arr[1] is the article number
+    selection = nil
+    until valid_selection?(selection)
+      puts "To go to a story, type the network name and then the article number (e.g., BBC 2)"
+      selection = gets.strip
+
+      selection = selection.split(" ")
+      if !valid_selection?(selection)
+        if selection.length == 2
+          selection[1] = selection[1].to_i
+        end
+      end
+
+      if !valid_selection?(selection)
+        puts "Invalid Entry"
+      end
+    end
+    binding.pry
+    selection
   end
 
   def retrieve_item
@@ -101,7 +130,7 @@ class CLIHeadlineScraper::CLI
   def retrieve_article
   end
 
-
+end
 
 #
 #
@@ -126,4 +155,3 @@ class CLIHeadlineScraper::CLI
 #
 #
 #
-end
