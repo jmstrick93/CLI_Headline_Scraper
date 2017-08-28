6af6db3631f672e63c9b0e5011ada48d195634ab
Jim Stricker <jmstricker93@gmail.com>
2017-08-28 15:06:30 -0400

    first three opening menu options added, begin making stub "make_selection" method.

--- lib/CLI_Headline_Scraper/CLI.rb
+++ lib/CLI_Headline_Scraper/CLI.rb
@@ -3,8 +3,23 @@
 class CLIHeadlineScraper::CLI
 
   def call
-    puts "Welcome Message"
-    puts "Selection options"
+    puts "Welcome to Headline Scraper"
+    puts "Please select which of the following headlines you would like to view:"
+
+    puts "  1. Main"
+    puts "  2. Politics"
+    puts "  3. Tech"
+
+    puts "Or type 'exit.'"
+
+    selection = gets.strip
+
+    make_selection(selection)
+
+  end
+
+  def make_selection(selection)
+    
   end
 
 end
--- lib/environment.rb
+++ lib/environment.rb
@@ -1,7 +1,2 @@
 require 'require_all'
-#will act as an "environment" file
 require_all "lib"
-
-module CLIHeadlineScraper
-  # Your code goes here...
-end
