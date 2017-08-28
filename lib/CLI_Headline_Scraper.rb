e35b68aead56d3e69c643274355f71cb96d29b40
Jim Stricker <jmstricker93@gmail.com>
2017-08-28 09:21:16 -0400

    finished initial functionality brainstorming, moving on to creating file structure

---
+++ lib/CLI_Headline_Scraper.rb
@@ -0,0 +1,5 @@
+require "CLI_Headline_Scraper/version"
+
+module CLIHeadlineScraper
+  # Your code goes here...
+end
---
+++ spec/CLI_Headline_Scraper_spec.rb
@@ -0,0 +1,11 @@
+require "spec_helper"
+
+RSpec.describe CLIHeadlineScraper do
+  it "has a version number" do
+    expect(CLIHeadlineScraper::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
