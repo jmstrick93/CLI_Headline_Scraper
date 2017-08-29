800c45633db9b028328367f71f49649de69b37f4
Jim Stricker <jmstricker93@gmail.com>
2017-08-29 13:19:31 -0400

    accidentally started working next phase without committing. committing now to correct this

---
+++ spec/2_Network_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Network do
+  let!(:network1){Network.new}
+
+  context "Initialization" do
+
+    it "initializes with a name" do
+
+    end
+
+    it "is saved when initialized" do
+
+    end
+
+    it "cannot have its name variable changed" do
+
+    end
+
+  end
+end
