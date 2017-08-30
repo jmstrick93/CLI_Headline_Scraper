84dddf1084c75f3b705940af3bfbbd5441ad305a
Jim Stricker <jmstricker93@gmail.com>
2017-08-30 12:24:14 -0400

    created the tests for Article Custom Creation context; created most tests for Article Finding an Article test

--- spec/3_Article_spec.rb
+++ spec/3_Article_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Article do
 
   context "Initialization" do
 
-    before(:each){Network.class_variable_set(:@@all, [])}
+    before(:each){Article.class_variable_set(:@@all, [])}
 
     it "accepts a @headline and @network_name variable upon initialization" do
       new_article = Article.new("fish are falling from the sky", "CNN")
@@ -26,10 +26,58 @@ RSpec.describe Article do
   end
 
   context "custom creation" do
+
+    before(:each){Article.class_variable_set(:@@all, [])}
+
     describe "#self.create_with_url" do
+      it "creates an article with url" do
+
+        article1 = Article.create_with_url("fish are falling from the sky", "CNN", "www.cnn.com/fish_article")
+
+        expect(article1.url).to eq("www.cnn.com/fish_article")
+
+      end
+
+      it "saves to @@all when initialized" do
+
+        new_article = Article.create_with_url("fish are falling from the sky", "CNN", "www.cnn.com/fish_article")
+        expect(Article.all).to include(new_article)
+
+      end
+
+    end
+
+    context "finding a headline" do
+      before(:each){Article.class_variable_set(:@@all, [])}
+
+      describe "#self.find_by_headline" do
+        it "returns the article with the headline given as argument" do
+          article1 = Article.new("fish are falling from the sky", "CNN")
+          article2 = Article.new("bananas are falling from the sky", "CNN")
+
+          found = Article.find_by_headline("bananas are falling from the sky")
+
+          expect(found).to be(article2)
+
+        end
+
+        it "returns nil when there is no article found" do
+          article1 = Article.new("fish are falling from the sky", "CNN")
+          article2 = Article.new("bananas are falling from the sky", "CNN")
+
+          found = Article.find_by_headline("raindrops are falling from the sky")
+
+          expect(found).to be_nil
+
+        end
+      end
+
+      describe "#self.find_by_network_name" do
+        it "returns an array of all articles belonging to the network with the given name" do
+
+        end
+      end
 
     end
   end
-
-
 end
