69a507a4b6200f0ec95d880050cb863c3a7050d2
Jim Stricker <jmstricker93@gmail.com>
2017-08-30 17:32:05 -0400

    created 4_CLI_article_selection_spec.rb file; added test stubs for 'selecting only a network' and 'selecting an article'

--- spec/4_CLI_article_selection_spec.rb
+++ spec/4_CLI_article_selection_spec.rb
@@ -1,1 +1,41 @@
 #will create tests with actuak Articles and Networks objects to assist me with making sure the CLI can interact properly with them.
+
+Rspec.describe CLI do
+
+  context 'Selecting only a network' do
+    describe '#go_to_homepage' do
+      it 'opens the network homepage in-browser' do
+
+      end
+
+    end
+  end
+
+  context 'Selecting an article' do
+    describe '#article_options' do
+      it "offers the user the choice to retrieve article text to display inline" do #if too much, display article metadata instead
+
+      end
+
+      it "offers the user the option to view the article in browser" do
+
+      end
+
+      it "lets the user return to previous menu" do
+
+      end
+
+      it "lets the user exit"
+
+    end
+
+
+  end
+
+
+
+
+
+
+
+end
