946ab02b631184b3feeec0817e18f4de45e397a7
Jim Stricker <jmstricker93@gmail.com>
2017-08-31 14:18:21 -0400

    moved let! and before statements to beginning of 4_CLI_article_selection_spec.rb

--- spec/4_CLI_article_selection_spec.rb
+++ spec/4_CLI_article_selection_spec.rb
@@ -6,17 +6,21 @@ RSpec.describe CLI do
 
   let!(:cli){CLI.new}
 
+  before(:each){Article.class_variable_set(:@@all, [])}
+  before(:each){Network.class_variable_set(:@@all, [])}
+
+  let!(:cnn){Network.create_with_url("CNN","http://www.cnn.com/")}
+
+  let!(:article1){Article.create_with_url("US, South Korea fly bombers over Korean Peninsula", "CNN", "http://www.cnn.com/2017/08/31/politics/us-bombers-korean-peninsula/index.html")}
+
+  let!(:article2){Article.create_with_url("Clean water runs out in Beaumont, Texas", "CNN", "http://www.cnn.com/2017/08/31/us/harvey-houston-texas-flood/index.html")}
+  
+  let!(:article3){Article.create_with_url("Texas Immigration Bill temporarily blocked", "CNN", "http://www.cnn.com/2017/08/30/politics/federal-judge-temporarily-blocks-sb4/index.html")}
+
+
+
   context 'When selecting only a network' do
     describe '#respond_to_selection' do
-
-      before(:each){Article.class_variable_set(:@@all, [])}
-      before(:each){Network.class_variable_set(:@@all, [])}
-
-      let!(:cnn){Network.create_with_url("CNN","http://www.cnn.com/")}
-      let!(:article1){Article.create_with_url("US, South Korea fly bombers over Korean Peninsula", "CNN", "http://www.cnn.com/2017/08/31/politics/us-bombers-korean-peninsula/index.html")}
-      let!(:article2){Article.create_with_url("Clean water runs out in Beaumont, Texas", "CNN", "http://www.cnn.com/2017/08/31/us/harvey-houston-texas-flood/index.html")}
-      let!(:article3){Article.create_with_url("Texas Immigration Bill temporarily blocked", "CNN", "http://www.cnn.com/2017/08/30/politics/federal-judge-temporarily-blocks-sb4/index.html")}
-
 
       it 'opens the network homepage in-browser' do
 
@@ -44,9 +48,9 @@ RSpec.describe CLI do
 
   context 'When selecting an article' do
     describe '#article_options_menu' do
-      it "offers the user the choice to retrieve article text to display inline"  #if too much, display article metadata instead
+      it "offers the user the choice to retrieve article text or to display inline" do  #if too much, display article metadata instead
 
-
+      end
 
       it "offers the user the option to view the article in browser"
 
